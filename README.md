6b4aca0afde82f475ae936b0b9541f3cc6c68bc1
Sadrac Tijerina <reavity3@gmail.com>
2021-04-15 09:28:09 -0500

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Code-Quiz
+# Code Quiz
+
+## Description
+
+This project I created a code quiz. Its a timed quiz where you can earn points depends on your answers as well as lose points and time.
+
+## Built With
+
+- HTML
+- CSS
+- Bootstrap
+- Javascript
+
+## Image
+
+![image of quiz question and answer choices](https://user-images.githubusercontent.com/20524736/114884377-57175180-9dcb-11eb-9404-db19efa1458e.png)
+
+## Website
+
+https://sadractijerina.github.io/Code-Quiz/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// A lot of global variables half of them being elements
 var startBtn = document.getElementById("startQuizBtn");
 var timer = document.getElementById("timer");
 var viewScore = document.getElementById("viewScore");
@@ -12,7 +13,9 @@ var answer2;
 var answer3;
 var answer4;
 var points = 0;
+var arrPoints = [];
 
+//array of objects containing the question, answer and the choices the user has.
 var questionsAndAnswers = [
   {
     question: "Commonly used data types DO Not include what",
@@ -49,6 +52,7 @@ var questionsAndAnswers = [
   },
 ];
 
+//This starts the quiz by removing the short paragraph describing the quiz and starting the time
 function startQuiz() {
   introduction.style.display = "none";
   timer.textContent = "Time: 50";
@@ -57,7 +61,7 @@ function startQuiz() {
   countdown();
 }
 
-// Timer that counts down from 60
+// Timer that counts down from 50
 function countdown() {
   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
@@ -80,10 +84,12 @@ function countdown() {
   displayQuestion();
 }
 
+//This function is used to display the questions and answers
 function displayQuestion() {
-  //debugger;
+  //Displays questions
   question.textContent = questionsAndAnswers[currentQuestion].question;
 
+  //Displays answers
   answer1 =
     "<button id='choice1' class='btn btn-primary btn-lg btn-block answerChoice'>" +
     questionsAndAnswers[currentQuestion].choices[0];
@@ -109,9 +115,25 @@ function displayQuestion() {
     "</li></ul>";
 }
 
+//This function is called everytime something is pressed in answers div
 function checkAnswer(event) {
   event.preventDefault();
   var button = document.getElementById(event.target.id);
+  var userInitials;
+
+  //the user at the end can press initals box but we dont want it to do anything when he does
+  if (button.id === "initials") {
+    return;
+  }
+
+  //if they press submit score we get the initials and saveScore function
+  if (button.id === "submitScore") {
+    userInitials = document.getElementById("initials");
+    saveScore(userInitials);
+    return;
+  }
+
+  //We are checking here the user answer and the correct answer and acting accordingly
   if (
     answer1.includes(questionsAndAnswers[currentQuestion].answer) &&
     button.id === "choice1"
@@ -152,8 +174,8 @@ function checkAnswer(event) {
     displayQuestion();
   }
 }
-function viewScores() {}
 
+// We are giving the user the chance to add their score
 function addScore() {
   timer.innerHTML = "";
   question.textContent = "All done!";
@@ -164,10 +186,50 @@ function addScore() {
   var initials =
     '<input id ="initials" type="text" name="initials" placeholder="" />';
 
-  var submitBtn = "<button id='submit' class='btn btn-primary >";
+  //var submitBtn = "<button id='submit' class='btn btn-primary >";
 
   answers.innerHTML =
-    "<h3>Enter initials" + initials + "</h3>" + submitBtn + "SUBMIT </button>";
+    "<h3>Enter initials: " +
+    initials +
+    "</h3>" +
+    '<button id="submitScore" class="btn btn-primary">SUBMIT</button>';
+}
+
+//we are saving score to cache
+function saveScore(userInput) {
+  console.log(userInput.value);
+
+  var userScore = {
+    initials: userInput.value,
+    score: points,
+  };
+
+  arrPoints.push(userScore);
+
+  localStorage.setItem("scores", JSON.stringify(arrPoints));
+}
+
+//Creates a table with the scores getting from the local storage, didnt have time to solve and complete this
+function viewScores() {
+  // var backButton;
+  // var cleanScoresButton;
+  // var readScores = localStorage.getItem("scores");
+  // var parseScores = JSON.parse(readScores);
+  // if (!parseScores) {
+  //   tableEls = "<h2>No scores saved!</h2>";
+  // } else {
+  //   parseScores.sort(compare);
+  //   var tableEls = "";
+  //   parseScores.forEach((element) => {
+  //     tableEls =
+  //       tableEls +
+  //       "<tr><td>" +
+  //       element.initials +
+  //       "</td><td>" +
+  //       element.score +
+  //       "</td></tr>";
+  //   });
+  // }
 }
 
 startBtn.addEventListener("click", startQuiz);
